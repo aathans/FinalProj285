08716cacd18dbe693bd816b408e476f437735077
Alex <aathans@umich.edu>
2014-11-18 22:36:54 -0500

    obstacle stuff

--- src/eecs285/proj4/finalGame/CarObstacle.java
+++ src/eecs285/proj4/finalGame/CarObstacle.java
@@ -1,5 +1,11 @@
 package eecs285.proj4.finalGame;
 
+import javax.imageio.ImageIO;
+import java.io.IOException;
+import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  * Created by Alex on 11/18/14.
  */
@@ -7,6 +13,15 @@ public class CarObstacle extends Obstacle {
 
     CarObstacle(){
         super();
+        setupCar();
+    }
 
+    private void setupCar(){
+        try {
+            URL carPath = this.getClass().getResource("/images/blue_car_mini.png");
+            obstacleImage = ImageIO.read(carPath);
+        } catch(IOException ex){
+            Logger.getLogger(CarObstacle.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
--- src/eecs285/proj4/finalGame/Obstacle.java
+++ src/eecs285/proj4/finalGame/Obstacle.java
@@ -1,5 +1,8 @@
 package eecs285.proj4.finalGame;
 
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
 /**
  * Created by Alex on 11/18/14.
  */
@@ -7,14 +10,22 @@ public class Obstacle {
 
     private int xPos;
     private int yPos;
-    private int obstacleHeight;
-    private int obstacleWidth;
+    protected int obstacleHeight;
+    protected int obstacleWidth;
+
+    protected BufferedImage obstacleImage;
 
     Obstacle(){
+        xPos = GameLogic.frameWidth/3 - 60;
+        yPos = 0;
+    }
+
+    public void update(Graphics2D g2d){
+        Draw(g2d);
 
     }
 
-    public void update(){
-
+    public void Draw(Graphics2D g2d){
+        g2d.drawImage(obstacleImage, xPos, yPos, obstacleWidth, obstacleHeight, null);
     }
 }
